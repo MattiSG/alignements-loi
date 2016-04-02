0a46deb294a7c093997a4cf2d4d7188e9ef02424
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-02 16:55:43 +0200

    Use variables instead of params

--- sources/openfisca.js
+++ sources/openfisca.js
@@ -1,15 +1,15 @@
 <openfisca>
 	<p>la variable
-	<input required type="text" name="query" placeholder="al.R1.taux1" value={ opts.value } onchange={ getDescription }>
+	<input required type="text" name="query" placeholder="nb_pac" value={ opts.value } onchange={ getDescription }>
 	dans Openfisca ({ description })</p>
 
 	<script>
 		getDescription() {
-			var url = 'http://api.openfisca.fr/api/1/parameters?name=' + this.query.value;
+			var url = 'http://api.openfisca.fr/api/1/variables?name=' + this.query.value;
 			fetch(url)
 				.then(function(response) { return response.json() })
 				.then(function(json) {
-					this.description = json.parameters[0].description;
+					this.description = json.variables[0].label;
 					this.root.value = url;
 					this.update();
 				}.bind(this), console.error.bind(console));
